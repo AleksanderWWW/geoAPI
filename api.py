7245ab9e839af3b2b94a678b81fd255a3eb50fef
AleksanderWWW <alwojnarowicz@gmail.com>
2022-08-18 09:56:32 +0200

    add dummy 'token' route

--- api.py
+++ api.py
@@ -1,5 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
 
+
+@app.route("/token", methods=["POST"])
+def create_token():
+    resp_body = {"hello": "world"}
+    return jsonify(resp_body)
+
+
+if __name__ == "__main__":
+    app.run()
