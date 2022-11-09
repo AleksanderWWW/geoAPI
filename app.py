6d49eea50e6878cbcfcb5cc1f8f40a84202cade0
Aleksander Wojnarowicz <alwojnarowicz@gmail.com>
2022-11-09 14:27:19 +0100

    move jwt expiry time value to constants module

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import os
-from datetime import timedelta
 
 from dotenv import load_dotenv
 
 from api import app
 from api.utils import get_mongo_collection
+from constants import JWT_TOKEN_EXPIRY_TIME_INTERVAL
 
 load_dotenv()
 
 app.config["SECRET_KEY"] = os.environ["APP_SECRET_KEY"]
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = JWT_TOKEN_EXPIRY_TIME_INTERVAL
 app.config["IP_STACK_KEY"] = os.environ["IP_STACK_KEY"]
 app.config["GEO_API_COLLECTION"] = get_mongo_collection(os.environ["MONGO_CONN_STR"], "Sofomo", "geoAPI")
 app.config["USERS_COLLECTION"] = get_mongo_collection(os.environ["MONGO_CONN_STR"], "Sofomo", "users")
---
+++ constants.py
@@ -0,0 +1,3 @@
+from datetime import timedelta
+
+JWT_TOKEN_EXPIRY_TIME_INTERVAL = timedelta(hours=1)
