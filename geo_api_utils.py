bee3293db415b0cd7fb854aa7a88993aa5f81aa4
AleksanderWWW <alwojnarowicz@gmail.com>
2022-11-14 22:02:54 +0100

    access connection string directly from env

--- geo_api_utils.py
+++ geo_api_utils.py
@@ -1,7 +1,10 @@
+import os
+
 import pymongo
 from pymongo.collection import Collection
 
 
-def get_mongo_collection(conn_str: str, db_name: str, coll_name: str) -> Collection:
+def get_mongo_collection(db_name: str, coll_name: str, conn_str = None) -> Collection:
+    conn_str = conn_str or os.environ["MONGO_CONN_STR"]
     client = pymongo.MongoClient(conn_str)
     return client[db_name][coll_name]
