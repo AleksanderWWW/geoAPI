33ebb4138ccd52a316732f4d186282ce40d2ab6a
AleksanderWWW <alwojnarowicz@gmail.com>
2022-11-14 22:01:51 +0100

    update app fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,31 @@
 import pytest
 
-from api import app
+from dotenv import load_dotenv
 
+from api import get_app
+from adapters.repository import (
+    MongoIPRepository,
+    MongoUserRepository,
+)
+
+from geo_api_utils import get_mongo_collection
 
 @pytest.fixture
 def myapp():
+    load_dotenv()
+    
+    user_repo = MongoUserRepository(
+        get_mongo_collection("Sofomo", "users")
+    )
+
+    ip_repo = MongoIPRepository(
+        get_mongo_collection("Sofomo", "geoAPI")
+    )
+
+    app = get_app(
+        user_repo=user_repo,
+        ip_repo=ip_repo
+    )
     app.config.update(
         {
             "TESTING": True,
@@ -18,4 +39,4 @@ def myapp():
 
 @pytest.fixture()
 def client(myapp):
-    return app.test_client()
+    return myapp.test_client()
